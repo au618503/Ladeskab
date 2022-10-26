3956caba0a8fb817d46c9ed9bb8b64a0fa5fd705
Marcin Szymanek <202009418@post.au.dk>
2022-10-26 19:35:37 +0200

    Updated the diagram Added ChargeControl interface (can test StationControl + Charger with it)

--- Lib/ChargeControl/IChargeControl.cs
+++ Lib/ChargeControl/IChargeControl.cs
@@ -1,6 +1,11 @@
-﻿namespace Cabinet_Library.ChargeControl;
+﻿using Cabinet_Library.ChargeControl.States;
+namespace Cabinet_Library.ChargeControl;
 
 public interface IChargeControl
 {
-
+    public void StartCharge();
+    public void StopCharge();
+    public bool DeviceConnected();
+    public void OnCurrentEvent(object? sender, ChargingEventArgs args);
+    public void ChangeState(StateBase state);
 }
