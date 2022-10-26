de16036064e1825f63f7406bebd4b2710d01e342
au618503 <58029607+au618503@users.noreply.github.com>
2022-10-26 23:12:50 +0200

    Test af ChargeControl, samt ændring af Vis til Show.

--- Test/ChargeControlTests/TestChargeControlInput.cs
+++ Test/ChargeControlTests/TestChargeControlInput.cs
@@ -14,6 +14,7 @@ namespace UnitTests.ChargeControlTests
     public class TestChargeControlInput
     {
         private IDisplay _display = Substitute.For<IDisplay>();
+        //private Display _display;
         private IStationControl _stationControl = Substitute.For<IStationControl>();
         private IUsbCharger _usbCharger;
         private ChargeControl _uut;
@@ -21,6 +22,7 @@ namespace UnitTests.ChargeControlTests
         public void Setup()
         {
             _usbCharger = Substitute.For<IUsbCharger>();
+            //_display = new Display();
             _uut = new ChargeControl(_usbCharger, _display);
         }
 
@@ -31,16 +33,19 @@ namespace UnitTests.ChargeControlTests
             _usbCharger.CurrentValue.Returns(6);
             _uut.StartCharge();
             _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = 6 });
-            Assert.AreEqual(StateID.CHARGING, _uut.GetState());
+            Assert.That(_uut.GetState(), Is.EqualTo(StateID.CHARGING));
         }
 
         [Test]
         public void Test_EventRaised_DisplayCalledSetCText()
         {
+            _usbCharger.Connected.Returns(false);
             _uut.StartCharge();
-            CurrentEventArgs testargs = new CurrentEventArgs() { Current = 10 };
+            CurrentEventArgs testargs = new CurrentEventArgs() { Current = 0};
             _usbCharger.CurrentValueEvent += Raise.EventWith(testargs);
-            _display.ReceivedWithAnyArgs().SetChargingText(default);
+            _display.Received().SetChargingText("Charging error. Contact support.");
+            //Assert.That(_display.SetChargingText(),Does.Contain("Test"));
+            //Assert.That(_display._stateText, Does.Contain("Test"));
         }
 
         [Test]
@@ -50,6 +55,33 @@ namespace UnitTests.ChargeControlTests
             _uut.StartCharge();
             _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = 530 });
             _usbCharger.Received().StopCharge();
+        }
+        [Test]
+        public void TestCharging_CurrentTooHigh_StateIsError()
+        {
+            _usbCharger.Connected.Returns(true);
+            _uut.StartCharge();
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = 530 });
+            Assert.That(_uut.GetState(), Is.EqualTo(StateID.ERROR));
+            
+        }
+
+        [Test]
+        public void TestCharging_StateIsFullyCharged()
+        {
+            _usbCharger.Connected.Returns(true);
+            _uut.StartCharge();
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = 0 });
+            Assert.That(_uut.GetState(), Is.EqualTo(StateID.FULLY_CHARGED));
+        }
+
+        [Test]
+        public void TestCharging_CurrentTooLow_StateIsReady()
+        {
+            _usbCharger.Connected.Returns(false);
+            _uut.StartCharge();
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = 0 });
+            Assert.That(_uut.GetState(), Is.EqualTo(StateID.READY));
         }
     }
 }
